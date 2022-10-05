bdf47ef4b74515e44135926d660a4f8b1c5957f4
unknown <smjoshua09@gmail.com>
2022-10-05 18:10:41 +0300

    commit after interview

--- db/migrate/20220721204628_require_shorter_cheets.rb
+++ db/migrate/20220721204628_require_shorter_cheets.rb
@@ -1,6 +1,16 @@
 class RequireShorterCheets < ActiveRecord::Migration[6.1]
   def up
       add_column :cheets, :message_short, :string, limit: 100
-      Cheet.where('char_length(message) > 100').update_all("message_short= substring(message, 97)||'...'")
+      cheets = Cheet.where('char_length(message) > 100')
+	  cheets.each do |p|
+	    new_msg = p.message; 
+	    p.update('message_short = ',new_msg[1,97]);
+	  
+	  cheets2 = Cheet.where('char_length(message) > 100')
+        cheets2.each do |q|
+	    new_msg2 = q.message; 
+	    p.update('message_short = ',new_msg2);		
+	  remove :cheets :message
+	  rename :cheets :message_short, :message 
   end
 end
